47ee3c78dd7b6dc0c4867b69d220fb3027849b22
Bruno <brunook13@outlook.com>
2022-01-24 19:58:33 -0300

    Finished project

---
+++ InterfaceHierarchy/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceHierarchy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Call from object level.
+            Rectangle myRectangle = new Rectangle();
+            myRectangle.Draw();
+            myRectangle.DrawInBoundingBox(10, 10, 100, 150);
+            myRectangle.DrawUpsideDown();
+            // Get IAdvancedDraw explicitly.
+            IAdvancedDraw iAdvDraw = myRectangle as IAdvancedDraw;
+            if (iAdvDraw != null)
+                iAdvDraw.DrawUpsideDown();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ InterfaceHierarchy/Rectangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceHierarchy
+{
+    class Rectangle : IAdvancedDraw
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Drawing a rectangle...");
+        }
+
+        public void DrawInBoundingBox(int top, int left, int bottom, int right)
+        {
+            Console.WriteLine("Drawing a rectangle inside a bounding box...");
+        }
+
+        public void DrawUpsideDown()
+        {
+            Console.WriteLine("Drawing a rectangle upside down...");
+        }
+    }
+}
